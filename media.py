4ee26c53d7b774cf9e9f726b17cd56894c1b39a4
soelo <43309307+soelo@users.noreply.github.com>
2018-09-16 11:49:33 -0500

    Add files via upload

--- media.py
+++ media.py
@@ -1,5 +1,5 @@
 import webbrowser
-
+'''creates a class Movie() to display on fresh_tomatoes website'''
 class Movie():
     ##list of instance variables for movie Class:
     def __init__(self,movie_title, release_date, movie_storyline, poster_image, trailer_youtube):
