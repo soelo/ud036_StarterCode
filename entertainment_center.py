fc720a56e5950bade37e6cafd2b4780920375008
soelo <43309307+soelo@users.noreply.github.com>
2018-09-18 22:04:43 -0500

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,8 @@
 import fresh_tomatoes
 import media
 
-tag = media.Movie("Tag","Friday, June 15",
+tag = media.Movie("Tag",
+                  "Friday, June 15",
                   "Friends keep a game of tag going for decades",
                   "https://upload.wikimedia.org/wikipedia/en/6/6f/Tag_%282018_film%29.png",
                   "https://www.youtube.com/watch?v=kjC1zmZo30U")
@@ -10,12 +11,14 @@ mm2 = media.Movie("Mamma Mia: Here We Go Again!",
                   "A sequel to the hit musical",
                   "https://upload.wikimedia.org/wikipedia/en/b/be/Mamma_Mia%21_Here_We_Go_Again.png",
                   "https://www.youtube.com/watch?v=XXGKPpK1Nhk&feature=player_embedded_uturn")
-spy = media.Movie("The Spy Who Dumped Me","Friday, August 3",
+spy = media.Movie("The Spy Who Dumped Me",
+                  "Friday, August 3",
                   "Audrey discovers her ex was a secret agent",
                   "https://upload.wikimedia.org/wikipedia/en/2/25/The_Spy_Who_Dumped_Me.png",
                   "https://www.youtube.com/watch?v=CXkUaaVrB_s")
 guernsey = media.Movie("The Guernsey Literary and Potato Peel Pie Society",
-                       "Friday, August 10","The story of a book club formed during WW2",
+                       "Friday, August 10",
+                       "The story of a book club formed during WW2",
                        "https://upload.wikimedia.org/wikipedia/en/2/28/The_Guernsey_Literary_and_Potato_Peel_Pie_Society.png",
                        "https://www.youtube.com/watch?v=HTDNGv61-Dk")
 asians = media.Movie("Crazy Rich Asians",
@@ -29,4 +32,5 @@ favor = media.Movie("A Simple Favor",
                     "https://upload.wikimedia.org/wikipedia/en/5/5c/A_Simple_Favor.png",
                     "https://www.youtube.com/watch?v=rAqMlh0b2HU")
 movies = [tag,mm2,spy,guernsey,asians,favor]
+# generate html and open page
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -3,7 +3,7 @@ import webbrowser
 
 
 class Movie():
-    # list of instance variables for movie Class:
+    # creating the list of instance variables for movie Class
     def __init__(self, movie_title, release_date, movie_storyline,
                  poster_image, trailer_youtube):
         self.title = movie_title
@@ -11,8 +11,7 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.date = release_date
-    # function to display trailer
 
+    # function to display trailer on mouse click
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
