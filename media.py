53855a693ca2905d1ecd527e3c74b1ce161dcd1a
soelo <43309307+soelo@users.noreply.github.com>
2018-09-20 20:40:29 -0500

    Add files via upload

--- media.py
+++ media.py
@@ -4,6 +4,11 @@ import webbrowser
 
 class Movie():
     # creating the list of instance variables for movie Class
+    # Variables: movie_title is the full name of the movie.
+    # movie_storyline is a brief sentence to describe the plot.
+    # poster_image is the whole URL for a poster image.
+    # trailer_youtube is the trailer video found on youtube.com.
+    # release_date is the date the movie was released in the United States.
     def __init__(self, movie_title, release_date, movie_storyline,
                  poster_image, trailer_youtube):
         self.title = movie_title
